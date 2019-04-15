d95e610115ee66534209fb4eb056097fc0b0c4e7
Valéria <valeria_padilha@yahoo.com.br>
2019-04-15 11:11:36 -0300

    Javascript

---
+++ 1. Introdução/Operações Aritméticas/meucodigo.js
@@ -0,0 +1,9 @@
+alert("JavaScript Aula 1");
+var a = 2.5;
+var b = 3;
+var c = a +b;
+var nome;
+alert("Total da soma: "+ c);
+document.write("Estou escrevendo no corpo da página");
+nome = prompt("Informe o seu nome");
+alert("Olá, "+ nome);
